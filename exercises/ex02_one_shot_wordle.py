30d0f5a52b6bec348b0810a2d2bc2c8fab3e0024
Amelia Spell <amspell@ad.unc.edu>
2022-09-07 20:48:37 -0400

    update ex_02

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,4 +1,4 @@
-"""One shot wordle exercise with emoji function"""
+"""One shot wordle exercise with emoji function."""
 
 __author__: str = "730560818"
 
@@ -14,26 +14,26 @@ GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
 while len(guess) != len(secret_word):
-    guess: str = str (input("That was not 6 letters! Try again: "))  
+    guess: str = str(input("That was not 6 letters! Try again: "))  
 while match < len(secret_word):
     while match < len(secret_word):
         if guess[match] == secret_word[match] and secret_word[alt]:
             result += f"{GREEN_BOX}"
-            """if index of guess is equal to index of secret word or alternate index of secret word green box prints"""
+            """if index of guess is equal to index of secret word or alternate index of secret word green box prints."""
         else: 
             while character_exist is not True and alt < len(secret_word):
                 if guess[match] == secret_word[alt]:
                     result += f"{YELLOW_BOX}"
                     alt = 0
                     character_exist = True
-                    """if guess index matches the alternate index of secret word but not orginal index yellow box prints"""
+                    """if guess index matches the alternate index of secret word but not orginal index yellow box prints."""
                 if guess[match] != secret_word[alt]:
                     alt = alt + 1
-                    """if guess index does not match alternate index of secret word test again using + 1 alternate index"""
+                    """if guess index does not match alternate index of secret word test again using + 1 alternate index."""
                 if alt >= len(secret_word):
                     result += f"{WHITE_BOX}"
                     character_exist = True
-                    """if guess index does not match any alternate or regular index of secret word white box prints"""
+                    """if guess index does not match any alternate or regular index of secret word white box prints."""
      
         match = match + 1
         alt = 0
