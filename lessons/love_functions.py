38bce78172edf09c0df0f877c2b6af11a91eb509
Amelia Spell <amspell@ad.unc.edu>
2022-09-13 19:02:48 -0400

    ex03 progress

---
+++ lessons/love_functions.py
@@ -0,0 +1,16 @@
+"""Given a subject as a parameter, returns a loving string."""
+def love(subject: str) -> str:
+    """Given a subject as a parameter, returns a loving string."""
+    return f"I love you {subject} !"
+
+print(love("amelia"))
+
+def spread_love(to: str, n:int) -> str:
+    """Generates a str repeating a loving message n times."""
+    love_note: str = ""
+    counter: int = 0
+    while counter < n:
+        love_note += love(to) + "\n"
+        counter += 1
+    return love_note
+
